01a0af4f04e179ba01cca5c98b3c4773139cb86b
ethanrdj <ethanrdavis96@gmail.com>
2020-10-23 19:42:16 +0100

    changed font for header

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -7,6 +7,7 @@ const Headers = () => {
       <img src={me} alt="me" className="ethan-pic"></img>
       <h1 className="name">Ethan Davis</h1>
       <h2 className="role">Junior Frontend Developer</h2>
+      <hr></hr>
     </div>
   );
 };
